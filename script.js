28bef0962852151e980a4be21bae5083df5611b8
RidhiC01 <117967614+RidhiC01@users.noreply.github.com>
2024-02-20 16:01:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // Add event listener to the login button
   loginButton.addEventListener('click', function() {
       // Redirect to the login page
-      window.location.href = "http://127.0.0.1:3000/hk1.html";
+      window.location.href = "hk1.html";
   });
 });
 

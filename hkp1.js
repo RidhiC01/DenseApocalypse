4c64dec5ef34f6c9e2fe8b3e69573299a283286a
princethats <160589194+princethats@users.noreply.github.com>
2024-02-20 15:42:52 +0530

    Add files via upload

--- hkp1.js
+++ hkp1.js
@@ -10,3 +10,15 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = 'http://127.0.0.1:3000/signup.html'; // Replace 'signup.html' with the URL of your sign-up page
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the sign-up button
+    var Home = document.getElementById('Home');
+
+    // Add click event listener to the sign-up button
+    Home.addEventListener('click', function() {
+        // Redirect to the sign-up page
+        window.location.href = 'http://127.0.0.1:3000/hk1.html'; // Replace 'signup.html' with the URL of your sign-up page
+    });
+});
+
